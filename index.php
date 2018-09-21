f44528add374af8eaec206e211fdb1ae4b4f7a18
Keegan Harris <keeganwharris@gmail.com>
2018-09-20 19:09:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<?php
+require('inc/functions.php');
+?>
+<!doctype html>
 <HTML>
 <head>
-<title>SCEDULE ROOM</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>SCEDULE ROOM</title>
 </head>
+  
 </HTML>
