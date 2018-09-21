aa5751a6440948f80e60c4b6870a696c8b8c15e9
Keegan Harris <keeganwharris@gmail.com>
2018-09-20 19:41:13 -0500

    Stuff
    more

--- index.php
+++ index.php
@@ -7,5 +7,10 @@ require('inc/functions.php');
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <title>SCHEDULE ROOM</title>
 </head>
-  
+<body>
+  <nav class="navbar navbar-full"></nav>
+<p>
+
+</p>
+</body>
 </HTML>
